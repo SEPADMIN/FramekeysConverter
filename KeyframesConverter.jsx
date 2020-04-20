6679a3e9278de29c35d8607b877ff74336e84556
SEPADMIN <32774955+SEPADMIN@users.noreply.github.com>
2020-04-20 20:17:49 +0700

    Update KeyframesConverter.jsx

--- KeyframesConverter.jsx
+++ KeyframesConverter.jsx
@@ -1,199 +1,76 @@
-﻿//FUNCTION SECTION...
-//debug section to be deleted...//
-//log wrapper
-log = function (arg)
-{
-    $.writeln(arg);
-}
-
-logLayersIndexes = function (layersIndexes_arr)
-{
-    var l_str = "";
-    if (layersIndexes_arr.length === 0)
-    {
-        log("empty");
-        return;
-    }
-    for (var i = 0; i < layersIndexes_arr.length; i++)
-    {
-        l_str += layersIndexes_arr[i] += " ";
-    }
-    log(l_str);
-}
-//...debug section to be deleted//
-
-//gets a URL based on the file path and the name 
-getUrl = function (name) 
-{
-    var projectName = app.project.file.name.replace(".aep", "");
-    var compName = app.project.activeItem.name;
-    var fileName = name || projectName + "_"+ compName + ".json";
-    fileName = fileName.replace(/\s/g, "");
-    var path = app.project.file.parent.absoluteURI + "/";
-    return path + fileName;
-}
-
-//   you need to allow AE to write files to your hard drive:
-//   go to: Edit > Preferences > General > and check on "Allow Scripts to Write Files and Access Network"
-//
-//write the output to disk:
-saveFile = function (obj, fileName) 
-{
-    var output = new File(getUrl(fileName + ".json"));
-    if (output.open("w")) 
-    {
-        output.encoding = "UTF-8";
-        var content =JSON.stringify(out, undefined, 2);
-        output.write(content);
-        output.close();
-    }
-}
-
-//recursive method to get all values from all layers
-scanLayers = function (obj, id, props) 
-{
-    if (id > comp.layers.length)
-    {
-        return;
-    }
-    
-    //for each layer 
-    var layer = comp.layer(id);
-    if (layer == null)
-    {
-        return;
-    }
-    
-    //stores all layer's values in an object
-    var tmp = {};
-    tmp.times = [];
-
-    //for each property  passed as an argument
-    for (var p = 0; p < props.length; p++) 
-    {
-        var property = layer[props[p]]; 
-        
-        //gets the keyframe count
-        var numKeys = property.numKeys; 
-        
-        //default value if property is not animated
-        if (numKeys <= 0) 
-        {
-            tmp[props[p]] = null;
-            continue;
-        }
-    
-        //if the property is animated 
-        var values = [];
-        if (numKeys > 0)
-        {
-            var time, value; 
-            for (var i = 1; i <= numKeys; i++) 
-            {
-                //gets the time and property values at this keyFrame
-                time = property.keyTime(i); 
-                value = property.keyValue(i);
-                
-                //stores them into an array which first value is the time stamp
-                var keyframe = [time];
-                if (!isNaN(value)) 
-                {
-                    // stores rotations in RADIANS
-                    if (props[p] == "rotation")  
-                    {
-                        keyframe.push(value * Math.PI / 180);
-                    }
-                    else
-                    {
-                        keyframe.push(value);
-                    }
-                }
-                else
-                {
-                    for(var j = 0; j < value.length; j++) 
-                    {
-                        keyframe.push(value[j]);
-                    }
-                }                
-                //store this keyframe in the layer object
-                values.push(keyframe);
-                
-                //if this timestamp was not yet present in the layer's list of timestamps
-                var addTime = true;
-                for (var j = 0; j < tmp.times.length; j++) 
-                {
-                    if (tmp.times[j] == time)
-                    {
-                        addTime = false;
-                    }
-                }
-                //add it
-                if (addTime) 
-                {
-                    tmp.times.push(time);
-                }
-            }
-        }
-
-        //stores the property's values for this layer and go to next property
-        tmp[props[p]] = values;
-      }
-  
-      //add this layer to the final object
-      obj[layer.name.replace(/\s/g, "_").toLowerCase()] = tmp; 
-      return scanLayers(obj, ++id, props);
-}
-//...FUNCTION SECTION
+//GLOBAL VARS SECTION...
+var _PROJ = app.project; 
+var _COMP = _PROJ.activeItem;
+var _PROPERTIES = [
+            "position", 
+            "rotation",
+            "scale"
+        ];
+var _fSelectedLayersIndexes_arr = [];
+//...GLOBAL VARS SECTION
 
 //GUI SECTION...
-var win = new Window("dialog", "Keyframes Converter", undefined);
-
-var lListboxContainer_panel = win.add("panel", undefined, "Container");
-lListboxContainer_panel.orientation = "row";
-
-var lSourceLayers_lb = lListboxContainer_panel.add("listbox", undefined);
-lSourceLayers_lb.alignment = ["fill", "fill"];
-lSourceLayers_lb.alignChildren = ["fill", "fill"];
-lSourceLayers_lb.bounds = { x:10, y:10, width:250, height:300 };
-
-var lDestLayers_lb = lListboxContainer_panel.add("listbox", undefined);
-lDestLayers_lb.alignment = ["fill", "fill"];
-lDestLayers_lb.alignChildren = ["fill", "fill"];
-lDestLayers_lb.bounds = { x:310, y:10, width:250, height:300 };
-
-var lSourceLayersIndexes_arr = [];
-var lDestLayersIndexes_arr = [];
-
-for (var i = 1; i <= comp.layers.length; i++)
+_buildGUI = function ()
 {
-    if (comp.layers[i] !== null)
+    var l_window = new Window("dialog", "Keyframes Converter", undefined);
+
+    var lListboxContainer_panel = l_window.add("panel", undefined, "Container");
+    lListboxContainer_panel.orientation = "row";
+
+    var lSourceLayers_lb = lListboxContainer_panel.add("listbox", undefined);
+    lSourceLayers_lb.alignment = ["fill", "fill"];
+    lSourceLayers_lb.alignChildren = ["fill", "fill"];
+    lSourceLayers_lb.bounds = { x:10, y:10, width:250, height:300 };
+
+    var lDestLayers_lb = lListboxContainer_panel.add("listbox", undefined);
+    lDestLayers_lb.alignment = ["fill", "fill"];
+    lDestLayers_lb.alignChildren = ["fill", "fill"];
+    lDestLayers_lb.bounds = { x:310, y:10, width:250, height:300 };
+
+    var lSourceLayersIndexes_arr = [];
+    var lDestLayersIndexes_arr = [];
+    _fSelectedLayersIndexes_arr = lDestLayersIndexes_arr;
+
+    for (var i = 1; i <= _COMP.layers.length; i++)
     {
-        lSourceLayers_lb.add("item", comp.layers[i].name);
-        lSourceLayersIndexes_arr.push(i);
+        if (_COMP.layers[i] !== null)
+        {
+            lSourceLayers_lb.add("item", _COMP.layers[i].name);
+            lSourceLayersIndexes_arr.push(i);
+        }
     }
+
+    lSourceLayers_lb.onDoubleClick = function ()
+    {
+        _moveSelectedLayerBetweenGroups(lSourceLayers_lb, lSourceLayersIndexes_arr, lDestLayers_lb, lDestLayersIndexes_arr);
+    }
+
+    lDestLayers_lb.onDoubleClick = function ()
+    {
+        _moveSelectedLayerBetweenGroups(lDestLayers_lb, lDestLayersIndexes_arr, lSourceLayers_lb, lSourceLayersIndexes_arr);
+    }
+
+    var lDescription_txt = l_window.add("statictext", undefined, "Double click on item to move it between groups");
+    lDescription_txt.alignment = ["fill", "fill"];
+
+    l_window.options = l_window.add("panel", undefined, "Settings", {name:"options"}); 
+
+    for (var i = 0; i < _PROPERTIES.length; i++)
+    {
+        l_window[_PROPERTIES[i]] = l_window.options.add("checkbox", undefined, _PROPERTIES[i], {name:"property_" + i}); 
+        l_window[_PROPERTIES[i]].value = true;
+    }
+
+    l_window.options.fileNameLabel = l_window.options.add("statictext", undefined, "json file name:"); 
+    l_window.options.fileName = l_window.options.add("edittext", undefined, "fileName");
+    l_window.options.convertBtn = l_window.options.add("button", undefined, 'Convert', {name:"ok"}); 
+
+    l_window.show(); 
+
+    return l_window;
 }
 
-lSourceLayers_lb.onDoubleClick = function ()
-{
-    moveSelectedLayerBetweenGroups(lSourceLayers_lb, lSourceLayersIndexes_arr, lDestLayers_lb, lDestLayersIndexes_arr);
-
-    //debug section to be deleted...//
-    /*logLayersIndexes(lSourceLayersIndexes_arr);
-    logLayersIndexes(lDestLayersIndexes_arr);*/
-    //...debug section to be deleted//
-}
-
-lDestLayers_lb.onDoubleClick = function ()
-{
-    moveSelectedLayerBetweenGroups(lDestLayers_lb, lDestLayersIndexes_arr, lSourceLayers_lb, lSourceLayersIndexes_arr);
-
-    //debug section to be deleted...//
-    /*logLayersIndexes(lSourceLayersIndexes_arr);
-    logLayersIndexes(lDestLayersIndexes_arr);*/
-    //...debug section to be deleted//
-}
-
-moveSelectedLayerBetweenGroups = function (aSrcLayersGroup_lb, aSourceLayersIndexes_arr, aDestLayersGroup_lb, aDestLayersIndexes_arr)
+_moveSelectedLayerBetweenGroups = function (aSrcLayersGroup_lb, aSourceLayersIndexes_arr, aDestLayersGroup_lb, aDestLayersIndexes_arr)
 {
     var lLayer_lbi = aSrcLayersGroup_lb.selection;
     if (lLayer_lbi === null)
@@ -208,53 +85,186 @@ moveSelectedLayerBetweenGroups = function (aSrcLayersGroup_lb, aSourceLayersInde
     aSrcLayersGroup_lb.remove(lLayer_lbi);
     aSourceLayersIndexes_arr.splice(lRelativeLayerIndex_num, 1);
 }
+//...GUI SECTION
 
-var lDescription_txt = win.add("statictext", undefined, "Double click on item to move it between groups");
-lDescription_txt.alignment = ["fill", "fill"];
-
-win.options = win.add("panel", undefined, "Settings"); 
-var prs = [
-        "position", 
-        "rotation",
-        "scale"
-    ];
-
-for (var i = 0; i < prs.length; i++)
+//FUNCTION SECTION...
+//debug section to be deleted...//
+//log wrapper
+log = function (a_str)
 {
-    win[prs[i]] = win.options.add("checkbox", undefined, prs[i]); 
-    win[prs[i]].value = true;
+    $.writeln(a_str);
+}
+//...debug section to be deleted//
+
+_getProperties = function (a_window)
+{
+    //specify properties to export
+    var lProperties_arr = [];
+    var lWinOptions_arr = a_window.children['options'];
+    for (var i = 0; i < _PROPERTIES.length; i++) 
+    {
+        lWinOptions_arr.children["property_" + i].value && lProperties_arr.push(_PROPERTIES[i]);
+    }
+    return lProperties_arr;
 }
 
-win.options.fileNameLabel = win.options.add("statictext", undefined, "json file name:"); 
-win.options.fileName = win.options.add("edittext", undefined, "fileName");
-win.options.convertBtn = win.options.add("button", undefined, 'Convert', {name:"ok"}); 
-
-win.show(); 
-
-//specify properties to export
-var properties = [];
-for (var i = 0; i < prs.length; i++) 
+_getFileName = function (a_window)
 {
-    //debug section to be deleted...//
-    //log(prs[i] + " -> " + win[prs[i]].value);
-    //...debug section to be deleted//
-    if (win[prs[i]].value) 
+    //specify file name or fall back to file name + .json
+    var lFileName_str = a_window.options.fileName.text;
+    if (lFileName_str == "fileName")
     {
-        properties.push(prs[i]);
+        lFileName_str = _PROJ.file.name.replace(".aep", "");
+    }
+    return lFileName_str;
+}
+
+//gets a URL based on the file path and the name 
+_getUrl = function (aURL_str) 
+{
+    var lProjectName_str = app.project.file.name.replace(".aep", "");
+    var lCompName_str = app.project.activeItem.name;
+    var lFileName_str = aURL_str || lProjectName_str + "_"+ lCompName_str + ".json";
+    lFileName_str = lFileName_str.replace(/\s/g, "");
+    var lPath_str = app.project.file.parent.absoluteURI + "/";
+    return lPath_str + lFileName_str;
+}
+
+//   you need to allow AE to write files to your hard drive:
+//   go to: Edit > Preferences > General > and check on "Allow Scripts to Write Files and Access Network"
+//
+//write the output to disk:
+_saveFile = function (aSrc_obj, lFileName_str) 
+{
+    var lOutput_file = new File(_getUrl(lFileName_str + ".json"));
+    if (lOutput_file.open("w")) 
+    {
+        lOutput_file.encoding = "UTF-8";
+        var lContent = JSON.stringify(aSrc_obj, undefined, 2);
+        lOutput_file.write(lContent);
+        lOutput_file.close();
     }
 }
 
-//specify file name or fall back to file name + .json
-var fileName = win.options.fileName.text;
-if (fileName == "fileName")
+_scanSelectedLayers = function (a_window, aDest_obj, aProperties_arr)
 {
-    fileName = app.project.file.name.replace(".aep", "");
+    var lLayersContainer_panel = a_window.children[0];
+    var lLayers_lb = lLayersContainer_panel.children[1];
+
+    if (!lLayers_lb || lLayers_lb.children.length === 0)
+    {
+        //log("No items selected");
+        return null;
+    }
+
+    var lTmp_obj = {};
+
+    //for each selected layer
+    for (var childIndex = 0; childIndex < lLayers_lb.children.length; childIndex++)
+    {
+        var lLayer = _COMP.layer(_fSelectedLayersIndexes_arr[childIndex]);
+        if (lLayer == null)
+        {
+            //log("layer skipped");
+            continue;
+        }
+
+        //for each property of selected layer
+        lTmp_obj[lLayer.name.replace(/\s/g, "_").toLowerCase()] = {};
+
+        var lProperties_arr = _getProperties(a_window);
+        for (var propIndex = 0; propIndex < lProperties_arr.length; propIndex++)
+        {
+            var l_property = lLayer[lProperties_arr[propIndex]];
+            var lNumKeys_int = l_property.numKeys;
+            if (lNumKeys_int <= 0)
+            {
+                //log("property skipped");
+                continue;
+            }
+
+            var lValues_arr = {};
+
+            //cut identical values from the end of the property
+            var lLastKey_int = lNumKeys_int;
+            while (
+                    (lLastKey_int > 1) &&
+                    (_areValuesEqual(l_property.keyValue(lLastKey_int), l_property.keyValue(lLastKey_int - 1)))
+                )
+            {
+                lLastKey_int--;
+            }
+
+            //for each timestamp of the property
+            for (var keyIndex = 1; keyIndex <= lLastKey_int; keyIndex++)
+            {
+                var lValue = l_property.keyValue(keyIndex);
+                var lTimestamp = l_property.keyTime(keyIndex);
+                lValues_arr[lTimestamp] = {};
+                if (lValue instanceof Array)
+                {
+                    switch(lProperties_arr[propIndex])
+                    {
+                        case "scale":
+                            lValues_arr[lTimestamp]["sx"] = (lValue[0] / 100).toFixed(2);
+                            lValues_arr[lTimestamp]["sy"] = (lValue[1] / 100).toFixed(2);
+                            break;
+                        case "position":
+                            lValues_arr[lTimestamp]["x"] = (lValue[0]).toFixed(2);
+                            lValues_arr[lTimestamp]["y"] = (lValue[1]).toFixed(2);
+                            break;
+                    }
+                }
+                else
+                {
+                    switch(lProperties_arr[propIndex])
+                    {
+                        case "rotation":
+                            lValues_arr[lTimestamp]["r"] = (lValue * Math.PI / 180).toFixed(2);
+                            break;
+                        case "alpha":
+                            lValues_arr[lTimestamp]["a"] = (lValue / 100).toFixed(2);
+                            break;
+                    }
+                }
+                lTmp_obj[lLayer.name.replace(/\s/g, "_").toLowerCase()][lProperties_arr[propIndex]] = lValues_arr;
+            }
+        }
+    }
+    return lTmp_obj;
 }
-//...GUI SECTION
+
+_areValuesEqual = function (aFirstValue, aSecondValue)
+{
+    if (
+            aFirstValue instanceof Array
+            || aSecondValue instanceof Array
+        )
+    {
+        if (aFirstValue.length !== aSecondValue.length)
+        {
+            return false;
+        }
+        for (var i = 0; i < aFirstValue.length; i++)
+        {
+            if (aFirstValue[i] !== aSecondValue[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    else
+    {
+        return (aFirstValue === aSecondValue) ? true : false;
+    }
+}
+//...FUNCTION SECTION
 
 //EXEC SECTION...
-var proj = app.project; 
-var comp = proj.activeItem; 
-var out = {};
-saveFile(scanLayers(out, 1, properties), fileName);
+var lOut_obj = {};
+var l_window = _buildGUI();
+var lProperties_arr = _getProperties(l_window);
+var lFileName_str = _getFileName(l_window);
+_saveFile(_scanSelectedLayers(l_window, lOut_obj, lProperties_arr), lFileName_str);
 //...EXEC SECTION
